647dd163fcc23fc9b24a2e281a6bef8ab5d957e4
elvis <elvis@dwenzi.com>
2011-08-26 00:41:24 +0800

    实现基本结构

--- ebgantt/ticketgantt.py
+++ ebgantt/ticketgantt.py
@@ -3,6 +3,7 @@ from genshi.builder import tag
 from trac.core import *
 from trac.web import IRequestHandler
 from trac.web.chrome import INavigationContributor, ITemplateProvider
+from pkg_resources import resource_filename
 
 class TicketGanttPlugin(Component):
     implements(INavigationContributor, IRequestHandler, ITemplateProvider)
@@ -10,22 +11,42 @@ class TicketGanttPlugin(Component):
     # INavigationContributor methods
     def get_active_navigation_item(self, req):
         return 'ticketgantt'
+
     def get_navigation_items(self, req):
         yield ('mainnav', 'ticketgantt',
             tag.a('Gantt Ticket', href= req.href.ticketgantt()))
+
+    def pre_process_request(self, req, handler):
+        # I think we should look for a TracJSGantt on the page and set
+        # a flag for the post_process_request handler if found
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+#        add_script(req, 'ebgantt/js/jsgantt.js')
+        return template, data, content_type
 
     # IRequestHandler methods
     def match_request(self, req):
         return req.path_info == '/ticketgantt'
 
     def process_request(self, req):
-        content = 'Hello World!'
-        req.send_response(200)
-        req.send_header('Content-Type', 'text/plain')
-        req.send_header('Content-Length', len(content))
-        req.end_headers()
-        req.write(content)
+        action = req.args.get('action')
+
+        if action == None:
+            re = self.show_main(req)
+        elif action == 'getData':
+            re = self.get_data(req)
+
+        return re
+
+    def show_main(self, req):
+        return 'gantt.html', {}, None
+
+    def get_data(self, req):
+        return 'ganttdata.xml', {}, 'test/xml'
 
     def get_templates_dirs(self):
-        from pkg_resources import resource_filename
         return [resource_filename(__name__, 'templates')]
+
+    def get_htdocs_dirs(self):
+        return [('ebgantt', resource_filename(__name__, 'htdocs'))]
